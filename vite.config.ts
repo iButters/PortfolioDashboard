3dfbddc5b9d29785054003f368faca03acd234fa
Sascha Siebrecht <s.siebrecht@iqul.de>
2026-01-05 22:53:51 +0100

    Ordnerstruktur angepasst

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/3d-portfolio-dashboard/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
